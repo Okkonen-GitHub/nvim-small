66654d6c95644128cd8d6ca284026c6cef24b705
Okko <60539807+Okkonen-GitHub@users.noreply.github.com>
2024-12-12 14:22:56 +0200

    init

---
+++ init.lua
@@ -0,0 +1,21 @@
+local o = vim.opt
+local g = vim.g
+local km = vim.keymap
+
+g.mapleader = " "
+
+o.mouse = 'a'
+o.number = true
+
+
+o.expandtab = true
+o.shiftwidth = 2
+o.tabstop = 2
+o.smartindent = true
+
+
+km.set("t", "<C-x>", "<C-\\><C-n>")
+km.set("v", "J", ":m '>+1<CR>gv=gv")
+km.set("v", "K", ":m '<-2<CR>gv=gv")
+km.set("n", "<C-u>", "<C-u>zz")
+km.set("n", "<C-d>", "<C-d>zz")
